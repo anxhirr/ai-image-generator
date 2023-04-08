69ccd7c852ade87f64491dd5bce6312fbebaf81a
anxhirr <anxhirr@gmail.com>
2023-04-08 11:43:12 +0200

    added great looking design for the imagesd. and the refresh button

--- components/Images.tsx
+++ components/Images.tsx
@@ -15,6 +15,7 @@ const Images = () => {
     mutate: refreshImages,
     error,
     isLoading,
+    isValidating,
   } = useSWR('/api/getImages', fetchImages, {
     revalidateOnFocus: false,
   })
@@ -22,10 +23,36 @@ const Images = () => {
   console.log('images', images)
 
   return (
-    <div>
-      <div>
-        {images?.imageUrls?.map((image: Image) => (
-          <div key={image.name}>
+    <div className=''>
+      <button
+        className='fixed bottom-10 right-10 bg-violet-400/90 text-white px-5 py-3 rounded-md 
+      hover:bg-violet-500 focus:outline-none focus:ring-2 focus:ring-violet-400 font-bold z-20'
+        onClick={() => refreshImages(images)}
+      >
+        {!isLoading && isValidating ? 'Refreshing...' : 'Refresh Images'}
+      </button>
+      {isLoading && (
+        <p className='animate-pulse text-center pb-7 font-extralight'>
+          Loading...
+          <span className='text-violet-400'> AI </span>
+          Generated Images
+        </p>
+      )}
+      <div className='grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 px-0 md:px-10'>
+        {images?.imageUrls?.map((image: Image, i: Number) => (
+          <div
+            className={`cursor-help relative 
+            hover:scale-[104%] transition-transform duration-200 ease-in-out
+            ${i === 0 ? 'md:col-span-2 md:row-span-2' : ''}`}
+            key={image.name}
+          >
+            {/* this will create a white div that will apear when hovered */}
+            <div className='absolute flex justify-center items-center w-full h-full bg-white opacity-0 hover:opacity-80 transition-opacity duration-200 z-10'>
+              {/* the line below remove the image time stapm and extension */}
+              <p className='text-center font-light text-lg p-5'>
+                {image.name.split('_').shift()?.toString().split('.').shift()}
+              </p>
+            </div>
             <Image
               src={image.url}
               alt={image.name}
